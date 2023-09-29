1820b01037b4221504c33d589c22a1c4ea31e962
Sushant Shukla <1805352@kiit.ac.in>
2023-09-29 14:49:45 +0530

    Production commit

--- index.js
+++ index.js
@@ -3,13 +3,35 @@ const {createServer} = require("http");
 const {Server} = require("socket.io");
 const cors = require("cors");
 
+const FRONTEND_URL = "http://localhost:3000";
+
 const app = express();
-app.use(cors({origin: 'https://localhost:3000'}));
+app.use(cors({origin: FRONTEND_URL}));
 const httpServer = createServer(app);
-const io = new Server(httpServer,{ cors: 'https://localhost:3000' });
+const io = new Server(httpServer,{ cors: FRONTEND_URL });
 
 io.on("connection", (socket) =>{
     console.log("connection setup");
+
+    socket.on("beginPath", (arg) => {
+        socket.broadcast.emit("beginPath",arg);
+    });
+
+    socket.on("drawLine", (arg) => {
+        socket.broadcast.emit("drawLine",arg);
+    });
+
+    socket.on("changeConfig",(arg) => {
+        socket.broadcast.emit("changeConfig",arg);
+    });
+
+    socket.on("menuItemClick",(arg) => {
+        socket.broadcast.emit("menuItemClick",arg);
+    });
+
+    socket.on("actionItemClick",(arg) => {
+        socket.broadcast.emit("actionItemClick",arg);
+    });
 });
 
 httpServer.listen(5000);
